def4ee430146e3ca959968437ff95f99c173b0c1
Cunhazão <57487885+jafcunha23@users.noreply.github.com>
2020-05-27 22:05:11 +0100

    27 cunha

---
+++ 27 cunha/ProjetoDA/ProjetoDA/Extensions/arrendamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDA
+{
+    public partial class Arrendamento {
+        public override string ToString()
+        {
+            return this.InicioContrato + " " + this.DuracaoMeses + " " + this.Renovavel + " " + this.Arrendatario.ToString();
+        }
+    }
+}
